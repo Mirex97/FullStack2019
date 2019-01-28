f46bfd1be86e207108a5b8346f260579a7fa22d3
Mirex97 <Mirex97@gmail.com>
2019-01-28 13:48:53 +0200

    Osa 1 d - unicafe step2

--- Osa (1)/unicafe/src/index.js
+++ Osa (1)/unicafe/src/index.js
@@ -12,22 +12,73 @@ const Statistics = props => {
 const Results = props => {
   return (
     <div>
-      <p>
-        {props.name} {props.result}
-      </p>
+      {props.name} {props.result} <br />
+    </div>
+  );
+};
+
+const Sum = function(values) {
+  const sum = values.good + values.neutral + values.bad;
+  return sum;
+};
+
+const Average = function(values) {
+  const average = values.good - values.bad;
+  const summa = Sum(values);
+  let x = average / summa;
+  if (isNaN(x)) {
+    x = 0;
+  }
+  return x;
+};
+
+const Positives = function(values) {
+  let x = (values.good / Sum(values)) * 100;
+  if (isNaN(x)) {
+    x = 0;
+  }
+  return x;
+};
+
+const Details = props => {
+  return (
+    <div>
+      yhteensä {Sum(props.feedback)} <br />
+      keskiarvo {Average(props.feedback)} <br />
+      positiivisia {Positives(props.feedback)} % <br />
     </div>
   );
 };
 
 const App = () => {
-  const buttonGood = () => {setGood(good + 1)};
-  const buttonNeutral = () => {setNeutral(neutral + 1)};
-  const buttonBad = () => {setBad(bad + 1)};
+  const buttonGood = () => {
+    const newFeedback = {
+      ...feedback,
+      good: feedback.good + 1
+    };
+    setFeedback(newFeedback);
+  };
+  const buttonNeutral = () => {
+    const newFeedback = {
+      ...feedback,
+      neutral: feedback.neutral + 1
+    };
+    setFeedback(newFeedback);
+  };
+  const buttonBad = () => {
+    const newFeedback = {
+      ...feedback,
+      bad: feedback.bad + 1
+    };
+    setFeedback(newFeedback);
+  };
 
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-  
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0
+  });
+
   return (
     <div>
       <Feedback />
@@ -35,9 +86,10 @@ const App = () => {
       <button onClick={buttonNeutral}>neutraali</button>
       <button onClick={buttonBad}>neutraali</button>
       <Statistics />
-      <Results name="hyvä" result={good} />
-      <Results name="neutraali" result={neutral} />
-      <Results name="huono" result={bad} />
+      <Results name="hyvä" result={feedback.good} />
+      <Results name="neutraali" result={feedback.neutral} />
+      <Results name="huono" result={feedback.bad} />
+      <Details feedback={feedback} />
     </div>
   );
 };
