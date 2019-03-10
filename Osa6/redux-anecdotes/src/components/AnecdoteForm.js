c6b13a3f8dc3184769f76c295c5449c5e2281a6d
Mirex97 <Mirex97@gmail.com>
2019-03-10 06:25:56 +0200

    6b teht 6.16-6.17

--- Osa6/redux-anecdotes/src/components/AnecdoteForm.js
+++ Osa6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { connect } from "react-redux";
 import { createAnecdote } from "../reducers/anecdoteReducer";
+import anecdoteService from "../services/anecdotes";
 
 const AnecdoteForm = props => {
-  const addAnecdote = event => {
+  const addAnecdote = async event => {
     event.preventDefault();
     const content = event.target.anecdote.value;
-    props.createAnecdote(content);
-    event.target.anecdote.value = "";
+    const newAnecdote = await anecdoteService.createNew(content);
+    props.createAnecdote(newAnecdote.content);
   };
 
   return (
